61efa51fb6f613511cbd90b7ae8df7ac2b40276a
Coconut153 <128516364+PlusMarden17@users.noreply.github.com>
2025-09-06 13:06:55 +0300

    Update Build.java

--- src/main/java/io/coconut/jlite/files/Build.java
+++ src/main/java/io/coconut/jlite/files/Build.java
@@ -1,1 +1,14 @@
+package io.coconut.jlite.files;
 
+import javax.swing.*;
+import java.io.*;
+
+public class Build {
+    public static void runCode() {
+      // TODO: Запуск коду в терміналі
+    }
+
+    public static void compileCode() {
+      // TODO: Компіляція коду
+    }
+}
